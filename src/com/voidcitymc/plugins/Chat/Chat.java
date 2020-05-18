bc04f720d42ac33d95c643439dbf31f08d5aac4c
ramdon-person <fierceeo@gmail.com>
2020-05-17 18:56:56 -0700

    fixed color code issue at the end and not getting the exact player

--- src/com/voidcitymc/plugins/Chat/Chat.java
+++ src/com/voidcitymc/plugins/Chat/Chat.java
@@ -21,20 +21,20 @@ public class Chat implements Listener {
         String[] newMessageA = new String[tempArray.length];
         String newMessage;
         
+        ChatColor lastColor;
         
         for (int i = 0; i < tempArray.length; i++) {
         	//check if a player is in the message ^
-        	if (Bukkit.getPlayer(tempArray[i]) != null) {
-            	newMessageA[i] = ChatColor.AQUA+tempArray[i]+ChatColor.WHITE;
-            	if (Bukkit.getPlayer(tempArray[i]) != null) {
-            		if (event.getRecipients().contains(Bukkit.getPlayer(tempArray[i]))) {
-            			//tell the player they got mentioned, tell sender they mentioned a player and play a ding noise
-            			Bukkit.getPlayer(tempArray[i]).playSound(Bukkit.getPlayer(tempArray[i]).getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 0.5F, 0F);
-            			Bukkit.getPlayer(tempArray[i]).sendMessage(ChatColor.AQUA+event.getPlayer().getName()+" has mentioned you!");
-            			event.getPlayer().sendMessage(ChatColor.AQUA+"You mentioned "+tempArray[i]+"!");
-            		} else {
-            			//could not mention player because of local chat plugin, etc.
-            		}
+        	if (Bukkit.getPlayerExact(tempArray[i]) != null) {
+            	if (event.getRecipients().contains(Bukkit.getPlayerExact(tempArray[i]))) {
+            		lastColor = ChatColor.getByChar(ChatColor.getLastColors(tempArray[i]));
+            		newMessageA[i] = ChatColor.AQUA+tempArray[i]+lastColor;
+            		//tell the player they got mentioned, tell sender they mentioned a player and play a ding noise
+            		Bukkit.getPlayerExact(tempArray[i]).playSound(Bukkit.getPlayer(tempArray[i]).getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 0.5F, 0F);
+            		Bukkit.getPlayerExact(tempArray[i]).sendMessage(ChatColor.AQUA+event.getPlayer().getName()+" has mentioned you!");
+            		event.getPlayer().sendMessage(ChatColor.AQUA+"You mentioned "+tempArray[i]+"!");
+            	} else {
+            		//could not mention player because of local chat plugin, etc.
             	}
         	} else {
         		newMessageA[i] = tempArray[i];
